c2fe8f8bc51ed523c4ac566d210960606ea9a1e6
Carola Casais <carola.casais@gmail.com>
2020-12-17 19:56:37 +0100

    first commit

---
+++ InMemory.Dal/Repository/StudentRepository.cs
@@ -0,0 +1,36 @@
+using InMemory.Dal.Models;
+using InMemory.Dal.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using static System.Console;
+
+namespace InMemory.Dal.Repository
+{
+    public class StudentRepository
+    {
+        private DbContextOptions<SchoolContext> _options;
+        public StudentRepository(DbContextOptions<SchoolContext> options)
+        {
+            _options = options;
+        }
+
+        public Student Add(Student student)
+        {
+            try
+            {
+                using (var context = new SchoolContext(_options))
+                {
+                    DbInitializer.Initialize(context);
+                    var result = context.Students.Add(student).Entity;
+                    context.SaveChanges();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ InMemory.Dal/Repository/TeacherRepository.cs
@@ -0,0 +1,55 @@
+using InMemory.Dal.Models;
+using InMemory.Dal.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using static System.Console;
+
+namespace InMemory.Dal.Repository
+{
+    public class TeacherRepository
+    {
+        private DbContextOptions<SchoolContext> _options;
+        public TeacherRepository(DbContextOptions<SchoolContext> options)
+        {
+            _options = options;
+        }
+
+        public Teacher Add(Teacher teacher)
+        {
+            try
+            {
+                using (var context = new SchoolContext(_options))
+                {
+                    DbInitializer.Initialize(context);
+                    var result = context.Teachers.Add(teacher).Entity;
+                    context.SaveChanges();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public Teacher Update(Teacher teacher)
+        {
+            try
+            {
+                using (var context = new SchoolContext(_options))
+                {
+                    DbInitializer.Initialize(context);
+                    var result = context.Teachers.Update(teacher).Entity;
+                    context.SaveChanges();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
